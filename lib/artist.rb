8db377c4a93d14ac89591c1bb2a50c5315f6d491
Mitchel Parish <mitchelparish@gmail.com>
2019-01-28 21:18:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,6 +11,12 @@ class Artist
     song.artist = self
   end
 
+  def add_song_by_name(name)
+    song = song.new(name)
+    @songs << song
+    song.artist = self
+  end
+
   def songs
     @songs
   end
