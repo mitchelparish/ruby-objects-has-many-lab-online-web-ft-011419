2d0f025aa06633f2c3585eed0de8c2f5da058974
Mitchel Parish <mitchelparish@gmail.com>
2019-01-28 21:27:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -9,17 +10,18 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-    @songs.count = number
-    number 
+    @@song_count
   end
 
 
