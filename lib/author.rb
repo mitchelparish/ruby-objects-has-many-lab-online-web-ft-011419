7197548589e7e56ee47f01f7844b6b0c29a5b3aa
Mitchel Parish <mitchelparish@gmail.com>
2019-01-28 22:16:03 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -4,14 +4,28 @@ class Author
 
   def initialize(name)
     @name = name
-    @posts = post
+    @posts = []
   end
 
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count += 1    
+    @@post_count += 1
   end
 
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+  def posts
+    @posts
+  end
 
 end
